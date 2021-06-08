cb5e416ac843c9f2913fc12198c249cc5eeffc58
soyalextreme <soyalextreme@gmail.com>
2021-06-07 20:25:48 -0500

    adding the styles and the layout

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -8,18 +8,21 @@ import Information from '../pages/Information';
 import Payment from '../pages/Payment';
 import Success from '../pages/Success';
 import NotFound from '../pages/NotFound';
+import Layout from '../components/Layout';
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/checkout" component={Checkout} />
-        <Route exact path="/checkout/information" component={Information} />
-        <Route exact path="/checkout/payment" component={Payment} />
-        <Route exact path="/checkout/success" component={Success} />
-        <Route component={NotFound} />
-      </Switch>
+      <Layout>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/checkout" component={Checkout} />
+          <Route exact path="/checkout/information" component={Information} />
+          <Route exact path="/checkout/payment" component={Payment} />
+          <Route exact path="/checkout/success" component={Success} />
+          <Route component={NotFound} />
+        </Switch>
+      </Layout>
     </BrowserRouter>
   );
 };
